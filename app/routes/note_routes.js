726ea3d88de0add153c0c38e0ea0e1f2ff9ec649
Dale Nguyen <dungnq@itbox4vn.com>
2017-06-16 22:54:19 -0500

    Add route for index.html

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,8 +1,21 @@
 // routes/note_routes.js
 
+var path = require('path')
+
 const JSON = require('circular-json');
 
 module.exports = function (app, db) {
+
+  app.get('/', function (req, res) {
+  	// Get file for the homepage
+  	var fileName = path.join(__dirname, '../../index.html');
+  	res.sendFile(fileName, function (err){
+  		if(err) console.log(err)
+  		else console.log('Sent', fileName)
+  	})
+  })
+
+
   app.get('/ads/:id', (req, res) => {
     const id = req.params.id;
     const details = { '_id': id };
