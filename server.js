44d8711b1adcc393c6ffa6138955ecd11fceb1b6
Dale Nguyen <dungnq@itbox4vn.com>
2017-06-16 22:26:20 -0500

    Add port listening 8080

--- server.js
+++ server.js
@@ -7,14 +7,14 @@ const db						= require('./config/db');
 
 const app = express();
 
-const port = 8000;
+const port = 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 MongoClient.connect(db.url, (err, database) => {
   if (err) return console.log(err)
   require('./app/routes')(app, database);
-	
+
   app.listen(port, () => {
     console.log('We are live on ' + port);
   });
