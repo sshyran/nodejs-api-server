114549db4d575cf8c3373478fe9d04be21e6c8fc
Dale Nguyen <dungnq@itbox4vn.com>
2017-06-16 22:14:08 -0500

    Return all data from MongoDB

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,8 +1,28 @@
 // routes/note_routes.js
 
+const JSON = require('circular-json');
+
 module.exports = function (app, db) {
-  app.post('/notes', (req, res) => {
-    console.log(req.body);
-    res.send('Hello');
-  })
+  app.get('/ads/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': id };
+
+    db.collection('oldmill_api').findOne(details, (err, item) => {
+      if(err){
+        res.send({'error': 'An error has occurred'});
+      } else {
+        res.send(item);
+      }
+    })
+  });
+
+  app.get('/ads', (req, res) => {
+    db.collection('oldmill_api').find().toArray(function(err, items) {
+      if(err){
+        res.send({'error': 'An error has occurred'});
+      } else {
+        res.send(JSON.stringify(items));
+      }
+    });
+  });
 };
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 // server.js
 
-const express = require('express');
-const MongoClient = require('mongodb').MongoClient;
-const bodyParser = require('body-parser');
+const express 			= require('express');
+const MongoClient 	= require('mongodb').MongoClient;
+const bodyParser 		= require('body-parser');
+const db						= require('./config/db');
 
 const app = express();
 
@@ -10,8 +11,11 @@ const port = 8000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(port, () => {
-	console.log('We are live on ' + port);
-});
-
-require('./app/routes')(app, {});
+MongoClient.connect(db.url, (err, database) => {
+  if (err) return console.log(err)
+  require('./app/routes')(app, database);
+	
+  app.listen(port, () => {
+    console.log('We are live on ' + port);
+  });
+})
