81e0ac223001aebe471e09f64dbb1b5e12c2da17
Dale Nguyen <dungnq@itbox4vn.com>
2017-06-16 23:12:43 -0500

    Changed port to app

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const db						= require('./config/db');
 
 const app = express();
 
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
